ee8e55028cbe4a9a703c29da71b2b15f31ea7734
reuben-idan <nobeadonis2@gmail.com>
2025-07-20 21:46:45 +0000

    Use posts.map instead of posts?.map for UserCard rendering in users page

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -14,7 +14,7 @@ const Users: React.FC<{ posts: UserProps[] }> = ({ posts }) => {
           </button>
         </div>
         <div className="grid grid-cols-3 gap-2 ">
-          {posts?.map((user: UserProps, key: number) => (
+          {posts.map((user: UserProps, key: number) => (
             <UserCard key={key} {...user} />
           ))}
         </div>
